813d599631b568ce9146badd76cd2ad7fa5dc40e
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-02-08 18:10:41 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -36,11 +36,41 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
-}
+  let binary = {
+    10: '.',
+    11: '-',
+    '**********': ' ',
+  }
+  function sliceToChunks(string, chunksSize) {
+      if (string[0] === '*') return [string]
+      resultArr = []
+      arr = string.split('')
+      for (let i = 0; i < string.length; i += chunksSize) {
+          const chunk = string.slice(i, i + chunksSize)
+          console.log(chunk)
+          resultArr.push(chunk)
+      }
+      return resultArr
+  }
+  let arrOfTen = sliceToChunks(expr, 10)
+  let cuttedArr = []
+  arrOfTen.forEach(el => {
+    let firstIndex = el.indexOf(1)
+    if (firstIndex < 1) cuttedArr.push(el)
+    else cuttedArr.push(el.slice(firstIndex, el.length + 1))
+  })
 
+return cuttedArr
+.map(el => sliceToChunks(el, 2))
+.map(el => el
+    .map(subEl => binary[subEl]))
+    .map(el => el.join(''))
+    .map(el => {
+        if (Object.keys(MORSE_TABLE).includes(el)) return MORSE_TABLE[el]
+        else return el
+    }).join('')
+}
 module.exports = {
     decode
 }
